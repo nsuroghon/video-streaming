9e79ce8ecb1ae18b2294855467e593a435e5ad17
niv suroghon <nsuroghon@gmail.com>
2023-10-12 14:33:29 -0400

    processing video file locally api functionality

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,31 @@
-import express from 'express'
+import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
-const PORT = 3000;
+app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send('hello world!')
+app.post('/process-video', (req,res) => {
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if(!inputFilePath || !outputFilePath){
+    res.status(400).send('Bad Request: Missing File Path');
+  }
+
+  // converting to 360p
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360")
+    .on("end", () => {
+      res.status(200).send("Processing finished succesfully.")
+    })
+    .on("error", (err) => {
+      console.log(`An error occured ${err.message} `);
+      res.status(500).send(`Internal Server Error ${err.message}`)
+    })
+    .save(outputFilePath);
 })
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
